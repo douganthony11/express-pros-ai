5516f0b0f254b3ed6b2d69095c65dc80dab6328d
douganthony11 <douganthony11@gmail.com>
2025-06-15 07:06:03 +0000

    add

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import os
 app = Flask(__name__)
 
 @app.route('/llm', methods=['POST'])
-def handle_llm_request():
-    data = request.json
+@app.route('/llm/<path:subpath>', methods=['POST', 'GET'])
+def handle_llm_request(subpath=None):
+    data = request.get_json() if request.is_json else {}
     
     # For now, just return a simple response to test
     return jsonify({
